13584e7f3bc5631a7d6db5e9ef6a6052b8b81112
Munish Sawhney <msawhney@wealthguardian.io>
2021-09-17 00:42:35 +0530

    About us and acknowledgements. .gitIgnore modifications

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,8 @@ const About = () => {
             <h1>About This App</h1>
             <p>App to search Github users</p>
             <p>Version: 1.0.0</p>
+            <p>Developed by: <small>Munish Sawhney</small></p>
+            <p>Acknowledgements: <small> <a href="https://www.traversymedia.com/" target="_blank">Traversy Media</a></small></p>
         </Fragment>
     )
 }
